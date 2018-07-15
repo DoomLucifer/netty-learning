695e9ec903760edc3ea162c8f1c84409acb0d126
zhengguoqiang <mail@zhengguoqiang.me>
2018-07-15 23:35:57 +0800

    first netty application

---
+++ echoclient/src/main/java/com/kyubi/netty/EchoClient.java
@@ -0,0 +1,11 @@
+package com.kyubi.netty;
+
+/**
+ * Created by zhengguoqiang on 2018/7/15.
+ */
+public class EchoClient {
+
+    public static void main(String[] args) {
+
+    }
+}
---
+++ echoserver/src/main/java/com/kyubi/netty/EchoServerHandler.java
@@ -0,0 +1,21 @@
+package com.kyubi.netty;
+
+import io.netty.buffer.ByteBuf;
+import io.netty.channel.ChannelHandler;
+import io.netty.channel.ChannelHandlerContext;
+import io.netty.channel.ChannelInboundHandlerAdapter;
+
+/**
+ * Created by zhengguoqiang on 2018/7/15.
+ */
+
+//标示一个ChannelHandler可以被多个 Channel 安全地共享
+@ChannelHandler.Sharable
+public class EchoServerHandler extends ChannelInboundHandlerAdapter {
+
+    @Override
+    public void channelRead(ChannelHandlerContext ctx, Object msg) throws Exception {
+        ByteBuf in = (ByteBuf) msg;
+
+    }
+}
